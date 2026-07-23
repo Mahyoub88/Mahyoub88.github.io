7cd8f15d4eba0cc41e94c4d15d2ad86d4ee3e148
Mohammed Mahyoub Ali Ayedh Mohammed <45149400+Mahyoub88@users.noreply.github.com>
2026-07-23 07:33:57 +0400

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -7,5 +7,5 @@ export function Container({
   children: ReactNode
   className?: string
 }) {
-  return <div className={`mx-auto w-full max-w-[1600px] px-6 lg:px-12 xl:px-16 ${className}`}>{children}</div>
+  return <div className={`mx-auto w-full max-w-[1800px] px-6 lg:px-14 xl:px-20 ${className}`}>{children}</div>
 }
